3b9fdeacab693a5535f251319d10d25b2aebe30a
Alok Agrawal <imalokagrawal@gmail.com>
2018-08-10 17:45:35 +0530

    Update assignPolicy.ps1

--- assignPolicy.ps1
+++ assignPolicy.ps1
@@ -1,3 +1,3 @@
 $mg = Get-AzureRmManagementGroup -GroupName 12345
 $definition = Get-AzureRmPolicyDefinition | Where-Object { $_.Properties.DisplayName -eq 'Demo Allowed locations' }
-New-AzureRmPolicyAssignment -Name "demo-assign-allowed-locations" -DisplayName "demo-assign-allowed-locations" -Scope $mg.Id -PolicyDefinition $definition -PolicyParameterObject @{listOfAllowedLocations=@("North Europe")}
+New-AzureRmPolicyAssignment -Name "demo-allowed-loc" -DisplayName "demo-allowed-loc" -Scope $mg.Id -PolicyDefinition $definition -PolicyParameterObject @{listOfAllowedLocations=@("North Europe")}
